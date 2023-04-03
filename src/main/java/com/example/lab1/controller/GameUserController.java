ef75804acee12fe9763d04f589e07965d4401a51
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-03 18:37:49 +0300

    removed api request

--- src/main/java/com/example/lab1/controller/GameUserController.java
+++ src/main/java/com/example/lab1/controller/GameUserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/gameusers")
+@RequestMapping("/gameusers")
 public class GameUserController {
 
     private final GameUserService gameUserService;
