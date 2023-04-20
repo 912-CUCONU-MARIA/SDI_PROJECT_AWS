2e3a750330e2578517eea6941ddf3149b901f564
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-20 18:01:44 +0300

    7.1 v4 changed query back, kept index

--- src/main/java/com/example/lab1/repository/GameUserRepository.java
+++ src/main/java/com/example/lab1/repository/GameUserRepository.java
@@ -11,29 +11,15 @@ import java.util.List;
 
 public interface GameUserRepository extends JpaRepository<GameUser,Long> {
 
-    //v1
-//    @Query(value = "SELECT g.id, g.active_status, g.email_address, g.first_name, g.last_name, g.password, g.username, AVG(pc.level) AS average_level " +
-//            "FROM game_user g " +
-//            "JOIN player_character pc ON g.id = pc.game_user_id " +
-//            "GROUP BY g.id " +
-//            "ORDER BY average_level DESC",
-//            countQuery = "SELECT COUNT(DISTINCT g.id) FROM game_user g JOIN player_character pc ON g.id = pc.game_user_id",
-//            nativeQuery = true)
-//    Page<Object[]> getGameUsersOrderedByAverageLevelOfPlayerCharacters(Pageable pageable);
-
-    @Query(value = "WITH avg_levels AS (" +
-            "  SELECT pc.game_user_id, AVG(pc.level) OVER (PARTITION BY pc.game_user_id) as avg_level " +
-            "  FROM player_character pc " +
-            "  GROUP BY pc.game_user_id, pc.level " +
-            ") " +
-            "SELECT g.id, g.active_status, g.email_address, g.first_name, g.last_name, g.password, g.username, al.avg_level " +
+    @Query(value = "SELECT g.id, g.active_status, g.email_address, g.first_name, g.last_name, g.password, g.username, AVG(pc.level) AS average_level " +
             "FROM game_user g " +
-            "JOIN avg_levels al ON g.id = al.game_user_id " +
-            "GROUP BY g.id, al.avg_level " +
-            "ORDER BY al.avg_level DESC",
+            "JOIN player_character pc ON g.id = pc.game_user_id " +
+            "GROUP BY g.id " +
+            "ORDER BY average_level DESC",
             countQuery = "SELECT COUNT(DISTINCT g.id) FROM game_user g JOIN player_character pc ON g.id = pc.game_user_id",
             nativeQuery = true)
     Page<Object[]> getGameUsersOrderedByAverageLevelOfPlayerCharacters(Pageable pageable);
 
+    
 
 }
